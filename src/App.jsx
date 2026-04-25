c0aa06fc9e7a7d7eadaee917f0ca10b1e7395f0b
amitchandradas2004 <amitcdas792@gmail.com>
2026-04-25 21:23:23 +0600

    Word on the street is added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Banner from "./Components/Banner/Banner";
 import Chart from "./Components/Chart/Chart";
 import Growth from "./Components/Growth/Growth";
 import Logo from "./Components/Logo/Logo";
+import Word from "./Components/Word/Word";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Chart></Chart>
       <Audience></Audience>
       <Growth></Growth>
+      <Word></Word>
     </>
   );
 }
---
+++ src/Components/Word/Word.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import SmallMan from "../../assets/smallman.png";
+import Table from "../../assets/table.png";
+const Word = () => {
+  return (
+    <div className="bg-linear-to-b from-[#CEFFB8] via-[#91F2CF] to-[#3CFFCE] py-10">
+      <div className="container  mx-auto">
+        <h3 className="text-5xl md:text-6xl font-bold text-center py-5">
+          Word on the street
+        </h3>
+        <div className="flex flex-col md:flex-row justify-between items-center md:h-80 lg:h-100 p-5 md:p-0 rounded-2xl bg-white overflow-hidden mx-5 md:mx-0">
+          <div className="left w-full md:w-1/2 space-y-4 pl-5">
+            <div>
+              <p className="text-xs lg:text-xl font-medium">
+                “The Proxima team has unparalleled consumer data that <br />
+                has very exciting applications post iOS 14.5. Not only <br />
+                is the data enriching to any brand’s targeting options, <br />
+                the team is very experienced and diligent in funneling <br />{" "}
+                paid media dollars to our various goals. Proxima has a <br />{" "}
+                great product and their team is super easy to work with!”
+              </p>
+            </div>
+            <div className="flex items-center space-x-2">
+              <span>
+                <img src={SmallMan} className="w-15 " />
+              </span>
+              <div>
+                <h5 className="text-2xl font-bold">Charlie Denby</h5>
+                <p className="font-medium">Director of Growth @ Canopy</p>
+              </div>
+            </div>
+          </div>
+          <div className="right w-full md:w-1/2 pt-5 md:pt-0">
+            <img
+              src={Table}
+              className="w-full h-full object-cover rounded-2xl md:rounded-none"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Word;
